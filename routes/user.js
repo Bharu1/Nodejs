3e6d5b763249e1b47bce3031132f31fa01b29c80
Bharu1 <abharathi1202@gmail.com>
2025-01-06 17:27:38 +0530

    Deleted unused files and updated user routes

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,9 @@ import { addUser, getUsers, getUserById } from "../controllers/user.js";
 
 const router = express.Router();
 
-router.post("/add-user", addUser);
-router.get("/user", getUsers);
-router.get("/user/:id", getUserById);
+router.post("/create/user", addUser);
+router.get("/get/users", getUsers);
+router.get("/get/user/:id", getUserById);
 
 router.get("/", (req, res) => {
   res.send("Welcome to the API! You can access users data at /user");
